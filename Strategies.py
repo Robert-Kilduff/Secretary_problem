fe22bd10611ae0c32f91eebb8a393be9d64c15ac
Robert-Kilduff <68850051+Robert-Kilduff@users.noreply.github.com>
2022-07-29 22:44:14 +0200

    Error handling & Data Transform

--- Strategies.py
+++ Strategies.py
@@ -51,19 +51,25 @@ def best_of_two():
     winner = winner.iat[0,1]
     return winner
 
-def look_leap(stop, list_len):
-    lst = list(np.arange(0,list_len))
-    random.shuffle(lst)
-    stop_nr = int((len(lst)/100) * stop)
-    print(len(lst))
-    print("stop ", stop)
-    print(stop_nr)
-    wait_group = lst[0:stop_nr]
-    pick_group = lst[stop_nr:]
+def look_leap(stop, list_len): #these lists should be pregenerated #TODO
+    lst = list(np.arange(0,list_len)) #create a list of unique numbers
+    random.shuffle(lst) #shuffle them
+    stop_nr = int((len(lst)/100) * stop) #the stop nr is a % of the total nr
+    wait_group = lst[0:stop_nr] #split the list into two, the first ending at the above nr
+    pick_group = lst[stop_nr:] #this starting at the end of the last list
     for i in pick_group:
-        if i < min(wait_group) and i <= min(pick_group[0:pick_group.index(i)]):
+        if i == 0:
             return i
         else:
-            pass
-    return 100
+            if pick_group.index(i) == 0:
+                if i < min(wait_group) and i <= min(pick_group[0:1]):
+                    return i
+                else:
+                    pass
+            else:    
+                if i < min(wait_group) and i <= min(pick_group[0:pick_group.index(i)]): #if i is the smallest nr that the loop has seen so far then return i, #index 0 is sadbad, change
+                    return i
+                else:
+                    pass
+    return 100 #this is the fail state where the loop never found a smaller number, therefore returns the worst result to indicate failure
 
